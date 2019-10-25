5859c2078bf3d32e9ec5483cb9b2c53df991070f
Justus Schock <justus.schock@rwth-aachen.de>
2019-10-25 15:03:34 +0200

    Add versioneer

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 import os
-import re
+import versioneer
 
 
 def resolve_requirements(file):
@@ -22,31 +22,17 @@ def read_file(file):
     return content
 
 
-def find_version(file):
-    content = read_file(file)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content,
-                              re.M)
-    if version_match:
-        return version_match.group(1)
-
-
 requirements = resolve_requirements(os.path.join(os.path.dirname(__file__),
                                                  'requirements.txt'))
 
 readme = read_file(os.path.join(os.path.dirname(__file__), "README.md"))
-license = read_file(os.path.join(os.path.dirname(__file__), "LICENSE"))
-_version = find_version(
-    os.path.join(
-        os.path.dirname(__file__),
-        "template-repo",
-        "__init__.py"))
 
 
 setup(
     name='template_package',
-    version=_version,
-    packages=find_packages(),
-    url='https://github.com/justusschock/template-repo-python',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    url='https://github.com/justusschock/delira-unet-baseline',
     test_suite="unittest",
     long_description=readme,
     long_description_content_type='text/markdown',
@@ -55,5 +41,5 @@ setup(
     python_requires=">=3.5",
     author="Justus Schock",
     author_email="justus.schock@rwth-aachen.de",
-    license=license,
+    license="MIT",
 )
